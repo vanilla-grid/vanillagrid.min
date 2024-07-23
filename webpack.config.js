55ff171721e11a7d203ddac3d37b51c7b38b91a5
hisondev <64723261+hisondev@users.noreply.github.com>
2024-07-24 08:35:26 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/Vanillagrid.js',
   output: {
-    filename: 'Vanillagrid.1.0.0.js',
+    filename: 'Vanillagrid.webpack.1.0.0.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'Vanillagrid',
     libraryTarget: 'umd',
