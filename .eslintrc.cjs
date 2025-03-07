dc5678a4da3ec6143750e228267d51b6d08e7454
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-07 16:57:04 +0900

    Create .eslintrc.cjs
    Change Typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: {            // 추가!
+        node: true    // Node.js 환경 인식
+    },
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-this-alias": "off",
+        "@typescript-eslint/ban-types": "off",
+        "no-useless-escape": "off",
+        "prefer-const": "off",
+        "no-prototype-builtins": "off",
+        "no-empty": "off",
+        "no-case-declarations": "off"
+    }
+};
